7dd026863c514c7ff7c23d7c9085b5e6c39dab98
pawas singh <pawas.kr.singh@gmail.com>
2026-02-04 17:21:26 +0530

    static update

--- HeyDayRealty/settings.py
+++ HeyDayRealty/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join('staticfiles')]
+STATIC_ROOT = os.path.join('static')
 
 # Media files
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
